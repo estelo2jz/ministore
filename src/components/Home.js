2d81da3748c74c6771dda646d913646d47706bcc
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-08-19 14:30:38 -0500

    work on home component and added placeholder images

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,15 +7,96 @@ export default class Home extends Component {
   render() {
     return (
       <div className="home-outer-container">
-        <div className="home-inner-container">
+        <div className="home-top-container">
           <div className="home-container">
-            <div className>
-              <h2>We Sell Everything!</h2>
+            <div className="home-header">
+              <div className="home-grid-container">
+                <div className="home-title-header">
+                  <h2>We Sell Everything!</h2>
+                </div>
+                <div className="home-top-grid-container">
+                  <div className="home-section-container">
+                    <div className="home-box-container">
+                      <div className="home-box-title-container">
+                        <h3>box 1</h3>
+                      </div>
+                      <div className="home-box-bio-container">
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="home-section-container">
+                    <div className="home-box-container">
+                      <div className="home-box-title-container">
+                        <h3>box 2</h3>
+                      </div>
+                      <div className="home-box-bio-container">
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="home-section-container">
+                    <div className="home-box-container">
+                      <div className="home-box-title-container">
+                        <h3>box 3</h3>
+                      </div>
+                      <div className="home-box-bio-container">
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div>
-          
+        <div className="home-bottom-container">
+          <div className="home-container">
+            <div className="home-grid-container">
+              <div className="home-image-container">
+                <img className="cart-product__image" src='http://via.placeholder.com/130x130' alt="image" />
+              </div>
+              <div className="home-image-container">
+                <img className="cart-product__image" src='http://via.placeholder.com/130x130' alt="image" />
+              </div>
+              <div className="home-image-container">
+                <img className="cart-product__image" src='http://via.placeholder.com/130x130' alt="image" />
+              </div>
+              <div className="home-image-container">
+                <img className="cart-product__image" src='http://via.placeholder.com/130x130' alt="image" />
+              </div>
+              <div className="home-image-container">
+                <img className="cart-product__image" src='http://via.placeholder.com/130x130' alt="image" />
+              </div>
+              <div className="home-image-container">
+                <img className="cart-product__image" src='http://via.placeholder.com/130x130' alt="image" />
+              </div>
+              <div className="home-image-container">
+                <img className="cart-product__image" src='http://via.placeholder.com/130x130' alt="image" />
+              </div>
+              <div className="home-image-container">
+                <img className="cart-product__image" src='http://via.placeholder.com/130x130' alt="image" />
+              </div>
+              <div className="home-image-container">
+                <img className="cart-product__image" src='http://via.placeholder.com/130x130' alt="image" />
+              </div>
+              <div className="home-image-container">
+                <img className="cart-product__image" src='http://via.placeholder.com/130x130' alt="image" />
+              </div>
+              <div className="home-image-container">
+                <img className="cart-product__image" src='http://via.placeholder.com/130x130' alt="image" />
+              </div>
+              <div className="home-image-container">
+                <img className="cart-product__image" src='http://via.placeholder.com/130x130' alt="image" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
